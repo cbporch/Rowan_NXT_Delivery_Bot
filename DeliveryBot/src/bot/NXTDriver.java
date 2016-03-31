82354fdd4d75825b1429917a01ac50c6e2406c89
porchc0 <cbporch@gmail.com>
2016-03-30 20:20:23 -0400

    added DiffPilot
    Signed-off-by: porchc0 <cbporch@gmail.com>

--- DeliveryBot/src/bot/NXTDriver.java
+++ DeliveryBot/src/bot/NXTDriver.java
@@ -3,7 +3,6 @@ package bot;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 
 import lejos.nxt.Button;
 import lejos.nxt.ButtonListener;
@@ -14,18 +13,14 @@ import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.Sound;
 import lejos.nxt.comm.Bluetooth;
 import lejos.nxt.comm.NXTConnection;
-import lejos.robotics.navigation.DifferentialPilot;
 import lejos.robotics.navigation.MoveController;
 import lejos.robotics.navigation.Navigator;
 import lejos.robotics.navigation.Pose;
-import lejos.robotics.navigation.Waypoint;
 import lejos.util.Delay;
 
 public class NXTDriver {
 	public static Navigator nav;
 	public static NXTConnection btConn;
-	private static ArrayList<Waypoint> intersections = new ArrayList<Waypoint>(10);
-	private static ArrayList<Waypoint> offices = new ArrayList<Waypoint>(10);
 	private static DataInputStream in;
 	private static DataOutputStream out;
 	private static NXTRegulatedMotor MotorA;
@@ -35,9 +30,15 @@ public class NXTDriver {
 		// initialize Navigator
 		MotorA = new NXTRegulatedMotor(MotorPort.A);
 		MotorB = new NXTRegulatedMotor(MotorPort.B);
-		nav = new Navigator(new DifferentialPilot(MoveController.WHEEL_SIZE_NXT1, MoveController.WHEEL_SIZE_NXT1, 13.75,
-				MotorA, MotorB, true));
-
+		DiffPilot pilot = new DiffPilot(MoveController.WHEEL_SIZE_RCX, 13.5, MotorA, MotorB, false);
+		nav = new Navigator(pilot);
+		
+		pilot.setAcceleration(250, 256);
+		pilot.setTravelSpeed(30, 30);
+//		MotorA.setAcceleration(256);
+//		MotorB.setAcceleration(250);
+//		MotorA.setSpeed(30);
+//		MotorB.setSpeed(15);
 		Button.ESCAPE.addButtonListener(new ButtonListener() {
 			public void buttonPressed(Button b) {
 				System.exit(0);
@@ -50,29 +51,43 @@ public class NXTDriver {
 
 		// UltrasonicSensor sonic = new UltrasonicSensor(SensorPort.S1);
 		System.out.println("\n\nInitializing..");
-		
+
 		// initialize Bluetooth connection
-		openConn();
-		selectState();
-		closeConn();
+		if(openConn()){
+			selectState();
+			closeConn();
+		}
 		
-		//insert code here to attempt to reconnect to bluetooth?
-		
+
+		// insert code here to attempt to reconnect to bluetooth?
+
 		System.out.println("Press any button to exit.");
 		Button.waitForAnyPress();
-		
+
 	}
-	
-	public static void openConn(){
+
+	public static boolean openConn() {
 		try {
 			btConn = Bluetooth.waitForConnection(60000, btConn.RAW);
 			in = new DataInputStream(btConn.openDataInputStream());
 			out = new DataOutputStream(btConn.openOutputStream());
 			LCD.clear();
-			System.out.println("Connected to the best phone ever!");
+			System.out.println("Connected");
 			Sound.beepSequenceUp();
+			return true;
 		} catch (NullPointerException e) {
 			System.out.println("No Connection");
+			return false;
+		}
+	}
+
+	private static void closeConn() {
+		try {
+			in.close();
+			out.close();
+			btConn.close();
+			Sound.beepSequence();
+		} catch (IOException e) {
 		}
 	}
 
@@ -81,134 +96,102 @@ public class NXTDriver {
 	 */
 	public static void selectState() {
 		int command = 0;
+
 		boolean exit = false;
 		while (!exit) {
 			command = readBTInput();
-			System.out.println(command);			
+			// System.out.println(command);
 			switch (command) {
-				case 0: // stop forward or backward movement
-					MotorA.stop(true);
-					MotorB.stop(true);
-					break;
-				case 1: // forward
-					MotorA.forward();
-					MotorB.forward();
-					break;
-				case 2: // backward
-					MotorA.backward();
-					MotorB.backward();
-					break;
-				case 3: // left
-					MotorA.forward();
-					MotorB.backward();
-					break;
-				case 4: // right
-					MotorA.backward();
-					MotorB.forward();
-					break;
-				case 5: // send current x and y
-					sendCoordinatesBT();
-					break;
-				case 6: // receive Coordinates
-					command = receiveCoordinates();
-					if(command == -1){
-						exit = true;
-					}
-					break;
-				case -1:// connection  lost
+			case 0: // stop forward or backward movement
+				MotorA.stop(true);
+				MotorB.stop(true);
+				nav.stop();
+				break;
+			case 1: // forward
+				// MotorA.forward();
+				// MotorB.forward();
+				nav.getMoveController().forward();
+				break;
+			case 2: // backward
+				// MotorA.backward();
+				// MotorB.backward();
+				nav.getMoveController().backward();
+				break;
+			case 3: // left
+				MotorA.forward();
+				MotorB.backward();
+				break;
+			case 4: // right
+				MotorA.backward();
+				MotorB.forward();
+				break;
+			case 5: // send current x and y
+				sendCoordinatesBT();
+				break;
+			case 6: // receive Coordinates
+				command = receiveCoordinates();
+				if (command == -1) {
 					exit = true;
-					break;
-				default:
-					break;
+				}
+				break;
+			case -1:// connection lost
+				exit = true;
+				break;
+			default:
+				break;
 			}
 		}
 	}
 
 	/*
-	 * Receive Coordinates from Bluetooth connection, add them to a path,
-	 * then follow it
+	 * Receive Coordinates from Bluetooth connection, add them to a path, then
+	 * follow it
 	 */
 	private static int receiveCoordinates() {
-		int command = readBTInput();
-			while(command != -2 || command!= -1){
-				float x = readBTInput(), 
-						y = readBTInput(), 
-						heading = readBTInput();
-				nav.addWaypoint(x, y, heading);
-			}
-			if (command == -1)
-				return command;
-			nav.followPath();
-			while(nav.isMoving()){
-				//check ultrasonic sensor
-				
-			}
-			// return to start
-			return 0;
-	}
-	
-
-	/*
-	 * Returns a Waypoint object for the current pose
-	 */
-	private static Waypoint getWaypoint() {
-		return new Waypoint(nav.getPoseProvider().getPose().getX(), nav.getPoseProvider().getPose().getY());
-	}
-
-	private static void bluetoothTest() {
-		boolean exit = false;
 		int command = 1;
-		while (!exit) {
-			try {
-				command = readBTInput();
-				if (command == 0 || command == -1) {
-					exit = true;
-					closeConn();
-					System.out.println("Goodbye!");
-				} else {
-					System.out.println(command);
-					Delay.msDelay(500);
-				}
-
-			} catch (Exception e) {
-				// no command read
-			}
+		System.out.println("Receiving Coordinates");
+		while (command > 0) {
+			int x = readBTInput(), y = readBTInput(), heading = readBTInput();
+			nav.addWaypoint(x, y, heading);
+			System.out.println("Waypoint Added");
+			command = readBTInput();
 		}
+		// if (command == -1){
+		// return command;
+		// }
+		System.out.println("Coord. Received");
+		nav.followPath();
+		// while(nav.isMoving()){
+		// check ultrasonic sensor
+		// }
+		// return to start
+		return 0;
 	}
 
 	private static int readBTInput() {
 		try {
-			// Delay.msDelay(60);
-			return in.read();
+			int out = in.readInt();
+			System.out.print(out + " ");
+			return out;
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			System.out.print("Read Error");
-			return 0;
+			return -1;
 		}
 	}
 
 	private static void sendCoordinatesBT() {
-		Delay.msDelay(500);
+		Delay.msDelay(50);
 		Pose p = nav.getPoseProvider().getPose();
 		float x = p.getX(), y = p.getY(), heading = p.getHeading();
 		try {
-			String output = Float.toString(x) +"\n"+Float.toString(y)+"\n"+Float.toString(heading)+"\n";
-			System.out.println("("+x+","+y+","+heading+")");
+			String output = Float.toString(x) + "\n" + Float.toString(y) + "\n" + Float.toString(heading) + "\n";
+			System.out.println("(" + x + "," + y + "," + heading + ")");
 			out.writeBytes(output);
 			out.flush();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			//e.printStackTrace();
-		}
-	}
-	
-	private static void closeConn() {
-		try {
-				in.close();
-				out.close();
-				btConn.close();
-				Sound.buzz();
-		} catch (IOException e) {
+			// e.printStackTrace();
 		}
 	}
 
@@ -235,5 +218,26 @@ public class NXTDriver {
 		Delay.msDelay(2000);
 		System.exit(0);
 
+	}
+
+	private static void bluetoothTest() {
+		boolean exit = false;
+		int command = 1;
+		while (!exit) {
+			try {
+				command = readBTInput();
+				if (command == 0 || command == -1) {
+					exit = true;
+					closeConn();
+					System.out.println("Goodbye!");
+				} else {
+					System.out.println(command);
+					Delay.msDelay(500);
+				}
+
+			} catch (Exception e) {
+				// no command read
+			}
+		}
 	}
 }
