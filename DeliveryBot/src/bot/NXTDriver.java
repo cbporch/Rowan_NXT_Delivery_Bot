080a35aa48c4bfd679e56670b54349f5366d6405
porchc0 <cbporch@gmail.com>
2016-04-22 14:00:34 -0400

    Mostly fixed.
    Signed-off-by: porchc0 <cbporch@gmail.com>

--- DeliveryBot/src/bot/NXTDriver.java
+++ DeliveryBot/src/bot/NXTDriver.java
@@ -9,7 +9,6 @@ import lejos.geom.Point;
 import lejos.nxt.Button;
 import lejos.nxt.ButtonListener;
 import lejos.nxt.LCD;
-import lejos.nxt.Motor;
 import lejos.nxt.MotorPort;
 import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.SensorPort;
@@ -17,17 +16,13 @@ import lejos.nxt.Sound;
 import lejos.nxt.addon.CompassHTSensor;
 import lejos.nxt.comm.Bluetooth;
 import lejos.nxt.comm.NXTConnection;
-import lejos.robotics.DirectionFinder;
 import lejos.robotics.localization.PoseProvider;
-import lejos.robotics.navigation.CompassPilot;
 import lejos.robotics.navigation.DifferentialPilot;
 import lejos.robotics.navigation.Navigator;
 import lejos.robotics.navigation.Pose;
 import lejos.robotics.navigation.Waypoint;
-import lejos.robotics.pathfinding.Path;
 import lejos.util.Delay;
 
-@SuppressWarnings("deprecation")
 public class NXTDriver {
 	public static Navigator nav;
 	public static DifferentialPilot pilot;
@@ -38,18 +33,22 @@ public class NXTDriver {
 	private static NXTRegulatedMotor MotorA;
 	private static NXTRegulatedMotor MotorB;
 	private static float _carpetTrackWidth;
+	@SuppressWarnings("unused")
 	private static float _tileTrackWidth;
 
+	/*
+	 * Initializes all objects, calibrates CompassHTSensor, 
+	 * and starts Bluetooth connections
+	 */
 	public static void main(String[] args) {
-		// initialize Navigator
-		_carpetTrackWidth = 15.0f;
+		// Smaller numbers rotate more
+		_carpetTrackWidth = 14.85f;
 		_tileTrackWidth = 15.86f;
 
 		MotorA = new NXTRegulatedMotor(MotorPort.A);
 		MotorB = new NXTRegulatedMotor(MotorPort.B);
 		pilot = new DifferentialPilot(2.5, _carpetTrackWidth, MotorA, MotorB, false);
 		compass = new CompassHTSensor(SensorPort.S1);
-		// cPilot = new CompassPilot(compass, 0, 0, MotorA, MotorB, false);
 		nav = new Navigator(pilot);
 		setMotorSpeeds();
 		Button.ESCAPE.addButtonListener(new ButtonListener() {
@@ -64,24 +63,21 @@ public class NXTDriver {
 
 		System.out.println("\n\nInitializing..");
 
-		// initialize Bluetooth connection
+		// initialize
 		if (openConn()) {
 			compass.startCalibration();
 			pilot.setRotateSpeed(20);
 			pilot.rotate(720, true);
 			while (pilot.isMoving()) {
-				//wait for completion
+				// wait for completion
 			}
 			compass.stopCalibration();
 			Delay.msDelay(50);
-			nav.goTo(0, 0, 0);
 			compass.resetCartesianZero();
-			Delay.msDelay(100);
-			
-			testCompassRotate();
+			Delay.msDelay(50);
 
-			selectState();
-			
+			test();
+//			selectState();
 			closeConn();
 		}
 
@@ -141,11 +137,7 @@ public class NXTDriver {
 		while (!exit) {
 			command = readBTInput();
 			setMotorSpeeds();
-			PoseProvider p = nav.getPoseProvider();
-			Pose pose = p.getPose();
-			pose.setHeading(compass.getDegreesCartesian());
-			p.setPose(pose);
-			nav.setPoseProvider(p);
+			resetHeading();
 			switch (command) {
 			case 0: // stop forward or backward movement
 				MotorA.stop(true);
@@ -201,52 +193,117 @@ public class NXTDriver {
 		}
 		while (command > 0) {
 			int x = readBTInput(), y = readBTInput(), heading = readBTInput();
-			System.out.println();
-			waypoints.add(new Waypoint(x,y,heading));
-//			nav.addWaypoint(x, y);
+			// System.out.println();
+			waypoints.add(new Waypoint(x, y, heading));
+			// nav.addWaypoint(x, y);
 			System.out.println("Waypoint Added");
 			command = readBTInput();
 		}
-	setMotorSpeeds();
+		setMotorSpeeds();
 		navigate(waypoints);
-//		nav.followPath();
-//		do {
-//			PoseProvider p = nav.getPoseProvider();
-//			Pose pose = p.getPose();
-//			pose.setHeading(compass.getDegreesCartesian());
-//			p.setPose(pose);
-//			nav.setPoseProvider(p);
-//		} while (nav.isMoving());
+		// nav.followPath();
+		// do {
+		// PoseProvider p = nav.getPoseProvider();
+		// Pose pose = p.getPose();
+		// pose.setHeading(compass.getDegreesCartesian());
+		// p.setPose(pose);
+		// nav.setPoseProvider(p);
+		// } while (nav.isMoving());
 		return 0;
 	}
-	private static void navigate(ArrayList<Waypoint> waypoints){
-		for(Waypoint w:waypoints){
-			compassRotate(nav.getPoseProvider().getPose().angleTo(new Point((float) w.getX(), (float) w.getY())));
-			nav.goTo(new Waypoint(w.getX(), w.getY()));
+
+	/*
+	 * Resets heading in PoseProvider, based on data from CompassHTSensor
+	 */
+	private static void resetHeading() {
+		PoseProvider posePr = nav.getPoseProvider();
+		Pose pose = posePr.getPose();
+		pose.setHeading(compass.getDegreesCartesian());
+		posePr.setPose(pose);
+		nav.setPoseProvider(posePr);
+	}
+
+	/*
+	 * Navigates a given ArrayList of Waypoint objects
+	 */
+	private static void navigate(ArrayList<Waypoint> waypoints) {
+
+		for (Waypoint w : waypoints) {
+			// returns x| -180<x<180
+			resetHeading();
+			float goal = nav.getPoseProvider().getPose().relativeBearing(new Point((float) w.getX(), (float) w.getY()));
+
+			System.out.println("heading:\n" + Math.round(nav.getPoseProvider().getPose().getHeading()) 
+											+ "\ngoal:\n" + Math.round(goal));
+			compassRotateToWaypoint(w);
+			nav.goTo((float)w.getX(), (float)w.getY());
 			nav.waitForStop();
 		}
+		Sound.twoBeeps();
 	}
-	
-	private static void compassRotate(float heading) {
+
+	/*
+	 * Rotates to an exact heading between 0 and 359.9
+	 */
+	private static void compassRotateTo(float goal) {
+		System.out.println("turning to:\n" + Math.round(goal));
 		PoseProvider posePr = nav.getPoseProvider();
-		while ((compass.getDegreesCartesian() > (heading + 0.5)) || 
-				(compass.getDegreesCartesian() < (heading - 0.5))) {
-			Pose pose = posePr.getPose();
-			pose.setHeading(compass.getDegreesCartesian());
-			posePr.setPose(pose);
-			nav.setPoseProvider(posePr);
-			
-			if (posePr.getPose().getHeading() != heading) {
-				float turnDegrees = heading - compass.getDegreesCartesian();
-				if(turnDegrees < -180){
+		while ((compass.getDegreesCartesian() > (goal + 1)) || (compass.getDegreesCartesian() < (goal - 1))) {
+			resetHeading();
+			System.out.println(Math.round(compass.getDegreesCartesian())
+								+ " " + Math.round(goal)
+								+ " " + Math.round((posePr.getPose().getHeading() - goal)));		
+			if (posePr.getPose().getHeading() != goal) {
+				float turnDegrees = (posePr.getPose().getHeading() - goal);
+				if (turnDegrees < -180) {
 					turnDegrees = 360 + turnDegrees;
+				} else if (turnDegrees > 180) {
+					turnDegrees = turnDegrees - 360;
 				}
-				pilot.rotate(-turnDegrees);
+				pilot.rotate(turnDegrees);
 				nav.waitForStop();
 			}
 		}
 	}
 
+	/*
+	 * Rotates to point in the direction of a given waypoint.
+	 */
+	private static void compassRotateToWaypoint(Waypoint w){
+		PoseProvider posePr = nav.getPoseProvider();
+		Point goal = new Point((float)w.getX(), (float)w.getY());
+		resetHeading();
+		// while current heading is not within 2 degrees of angle to goal
+		float angleTo = posePr.getPose().angleTo(goal);
+		if(angleTo < 0){
+			angleTo = 360 + posePr.getPose().angleTo(goal);
+		}else{
+			angleTo = posePr.getPose().angleTo(goal);
+		}
+		while((angleTo < compass.getDegreesCartesian() - 1.0f) ||
+				(angleTo > compass.getDegreesCartesian() + 1.0f)){
+			resetHeading();
+			System.out.println(Math.round(compass.getDegreesCartesian())
+					+ " " + Math.round(angleTo)
+					+ " " + Math.round(-posePr.getPose().relativeBearing(goal)));
+			
+			pilot.rotate(-posePr.getPose().relativeBearing(goal));
+			while(pilot.isMoving()){
+				if(compass.getDegreesCartesian() == angleTo){
+					pilot.stop();
+				}
+			}
+			if(posePr.getPose().angleTo(goal) < 0){
+				angleTo = 360 + posePr.getPose().angleTo(goal);
+			}else{
+				angleTo = posePr.getPose().angleTo(goal);
+			}
+		}
+	}
+	
+	/*
+	 * Reads Bluetooth input and returns an int
+	 */
 	private static int readBTInput() {
 		try {
 			int out = in.readInt();
@@ -258,11 +315,15 @@ public class NXTDriver {
 		}
 	}
 
+	/*
+	 * Sends current pose information over Bluetooth
+	 */
 	private static void sendCoordinatesBT() {
 		// Delay.msDelay(50);
 		Pose p = nav.getPoseProvider().getPose();
 		float x = p.getX(), y = p.getY(), heading = p.getHeading();
 		try {
+			// Current x, y, heading
 			String output = Float.toString(x) + "\n" + Float.toString(y) + "\n" + Float.toString(heading) + "\n";
 			System.out.println("(" + x + "," + y + "," + heading + ")");
 			out.writeBytes(output);
@@ -272,68 +333,58 @@ public class NXTDriver {
 		}
 	}
 
+	/*
+	 * Tests compassRotateTo method. Should rotate NXT to 90 degrees and maintain heading
+	 */
+	@SuppressWarnings("unused")
 	private static void testCompass() {
-		
 		while (true) {
-			PoseProvider posePr = nav.getPoseProvider();
-			Pose pose = posePr.getPose();
-			pose.setHeading(compass.getDegreesCartesian());
-			posePr.setPose(pose);
-			nav.setPoseProvider(posePr);
-			
-			System.out.println(posePr.getPose().getHeading() +" vs. "+compass.getDegreesCartesian());
-			
-			if (posePr.getPose().getHeading() != 90.0f) {
-				float turnDegrees = 90 - compass.getDegreesCartesian();
-				if(turnDegrees < -180){
-					turnDegrees = 360 + turnDegrees;
-				}
-				pilot.rotate(-turnDegrees);
-				nav.waitForStop();
-			}
+			compassRotateTo(90);
 		}
 	}
 
-	private static void testCompassRotate(){
-		do{
+	/*
+	 * Tests compassRotateTo method. Should turn 90 degrees, beep twice, then continue
+	 * that pattern. (Turn, beep, turn, beep, etc.)
+	 */
+	@SuppressWarnings("unused")
+	private static void testCompassRotate() {
+		do {
 			System.out.println("Start test");
-			compassRotate(0);
+			compassRotateTo(0);
 			Sound.twoBeeps();
-			compassRotate(90);
+			compassRotateTo(90);
 			Sound.twoBeeps();
-			compassRotate(180);
+			compassRotateTo(180);
 			Sound.twoBeeps();
-			compassRotate(270);
+			compassRotateTo(270);
 			Sound.twoBeeps();
-		}while(true);
+		} while (true);
 	}
-	
+
+	/*
+	 * Tests navigate method. Should drive in a square continuously.
+	 */
 	@SuppressWarnings("unused")
 	private static void test() {
-		nav.getMoveController().forward();
-		Delay.msDelay(2000);
-		nav.stop();
-		Delay.msDelay(2000);
-		nav.getMoveController().backward();
-		Delay.msDelay(2000);
-		nav.stop();
-		Delay.msDelay(2000);
-		Motor.A.backward();
-		Motor.B.forward();
-		Delay.msDelay(2000);
-		Motor.A.stop(true);
-		Motor.B.stop(true);
-		Delay.msDelay(2000);
-		Motor.B.backward();
-		Motor.A.forward();
-		Delay.msDelay(2000);
-		Motor.A.stop(true);
-		Motor.B.stop(true);
-		Delay.msDelay(2000);
-		System.exit(0);
-
+		 ArrayList<Waypoint> waypoints = new ArrayList<Waypoint>();
+		 waypoints.add(new Waypoint(0,20));
+		 waypoints.add(new Waypoint(20,20));
+		 waypoints.add(new Waypoint(20,0));
+		 waypoints.add(new Waypoint(0,0));
+		
+		 while(true) {
+			 navigate(waypoints);
+		 }
+//		while (true) {
+//			resetHeading();
+//			System.out.println(nav.getPoseProvider().getPose().getHeading());
+//		}
 	}
 
+	/*
+	 * Tests readBTInput method.
+	 */
 	@SuppressWarnings("unused")
 	private static void bluetoothTest() {
 		boolean exit = false;
